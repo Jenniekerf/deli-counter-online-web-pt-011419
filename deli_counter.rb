56b9b2fc156662b473bcd494085beff43a9783cd
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-21 17:38:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli.size == 0 
+    puts "The line is currently empty."
+  else
+    deli.each.with_index(1) do |name, i|
+    puts "The line is currently: #{i}. #{name}" 
+  end
+end
+end
