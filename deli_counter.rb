63a7492b697ceb1ec9053ac320f083c99bfab95a
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-01-21 17:42:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,8 @@ def line(deli)
   if deli.size == 0 
     puts "The line is currently empty."
   else
-    deli.each.with_index(1) do |name, i|
-   puts katz_deli << #{i}. #{name}" 
-    end
+    deli.each.with_index(1) {|name, i|
+   puts katz_deli << #{i}. #{name}"} 
 end
 
 end
